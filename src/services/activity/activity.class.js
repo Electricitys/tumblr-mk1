402474b9b19cf3d8b3ecd23f01b864c4331ce278
Imanuel Pundoko <ilomon10@gmail.com>
2021-08-12 20:41:09 +0800

    Refactor

--- src/services/activity/activity.class.js
+++ src/services/activity/activity.class.js
@@ -7,25 +7,25 @@ exports.Activity = class Activity extends Service {
     this.app = app;
   }
   async create(data, params) {
-    if (!params.user["patient.id"]) throw new BadRequest("User not available.");
-    if (!params.user["patient.device.id"]) throw new BadRequest("Device not available.");
+    if (!params.user['patient.id']) throw new BadRequest('User not available.');
+    if (!params.user['patient.device.id']) throw new BadRequest('Device not available.');
     return super.create({
-      deviceId: params["user"]["patient.device.id"],
-      patientId: params["user"]["patient.id"],
+      deviceId: params['user']['patient.device.id'],
+      patientId: params['user']['patient.id'],
       ...data
     }, params);
   }
   async find(params) {
-    if (params.query["$include"]) {
-      params["sequelize"] = {
-        include: params.query["$include"].map(({ model, as }) => {
+    if (params.query['$include']) {
+      params['sequelize'] = {
+        include: params.query['$include'].map(({ model, as }) => {
           return {
             model: this.app.service(model).Model,
             as: as
-          }
+          };
         })
-      }
-      delete params.query["$include"];
+      };
+      delete params.query['$include'];
     }
     return super.find(params);
   }
